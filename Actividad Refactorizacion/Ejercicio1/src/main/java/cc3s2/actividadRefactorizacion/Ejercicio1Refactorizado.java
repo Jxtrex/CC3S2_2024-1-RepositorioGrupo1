d92aad8d1d31961db16101b7307c97e03af1ac72
Lerzundi <juan.lerzundi.r@uni.pe>
2024-05-28 20:23:48 -0500

    Update

--- Actividad Refactorizacion/Ejercicio1/src/main/java/cc3s2/actividadRefactorizacion/Ejercicio1Refactorizado.java
+++ Actividad Refactorizacion/Ejercicio1/src/main/java/cc3s2/actividadRefactorizacion/Ejercicio1Refactorizado.java
@@ -1,4 +1,87 @@
 package cc3s2.actividadRefactorizacion;
 
-public class Ejercicio1Refactorizado {
+import java.util.ArrayList;
+import java.util.List;
+
+class Ejercicio1Refactorizado {
+    public class Employee {
+        private String name;
+
+        Employee(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public class Department {
+        String department;
+        List<Employee> Empleados = new ArrayList<>();
+
+        Department(String department) {
+            this.department = department;
+        }
+
+        public String getDepartment() {
+            return department;
+        }
+        
+    }
+
+    public class EmployeeManager {
+        List<Employee> Empleados = new ArrayList<>();
+        List<Department> Departamentos = new ArrayList<>();
+
+        public void addEmployee(String name, String department) {
+            // Instanciamos un objeto Employee("name")
+            Employee empleado = new Employee(name);
+            Department departamento = new Department(department)
+            // Buscamos un objeto Department(department: "department")
+            if(Departamentos.contains(departamento))
+                {
+                // Añadimos un Employee(name : "name") a Department(department: "department")
+                Departamentos.get(Departamentos.indexOf(departamento)).Empleados.add(empleado);
+                System.out.println("Empleado añadido");
+        
+                }
+            else 
+               { 
+                System.out.println("No existe el departamento");
+                break;
+                }    
+            }
+
+        public void removeEmployee(String name) {
+            // TODO Elimina un empleado
+            // Buscamos un objeto Employee(name: "name")
+            // FIXME: Falta implementar, interrupción no prevista
+            // Eliminamos Employee(name: "name")
+            System.out.println("Empleado eliminado");
+        }
+
+        public void changeDepartment(String employeeName, String newDepartment) {
+            // TODO Cambia un empleado de departamento
+            // Buscamos un objeto Employee(name: "employeeName")
+            // Buscamos un objeto Department(department: "newDepartment")
+            // Movemos Employee(name: "name") a Department(department: "newDepartment")
+            System.out.println("Departamento cambiado");
+        }
+
+        public void printDepartmentReport(String department) {
+            // TODO Imprime un informe del departamento
+            // Buscamos Department(department: "department")
+            // Usamos el método Department::toString
+            System.out.println("Informe del departamento " + department);
+        }
+
+        public void printAllDepartments() {
+            // TODO Imprime todos los departamentos
+            // Iteramos sobre la colección de Department y aplicamos Department::toString
+            // para cada elemento
+            System.out.println("Lista de todos los departamentos");
+        }
+    }
+
 }
--- Actividad Refactorizacion/README.md
+++ Actividad Refactorizacion/README.md
@@ -5,6 +5,10 @@
 ## Ejercicio 1
 ### Análisis Inicial del Código Fuente
 **Métricas de Cohesión**  
+La cohesión se refiere a la medida en que las tareas realizadas por un solo módulo son funcionalmente
+relacionadas. Una alta cohesión dentro de un módulo es deseable porque significa que el módulo realiza
+una tarea bien definida y no está sobrecargado con responsabilidades que no están estrechamente
+relacionadas.  
 **LCOM**
 >[!NOTE]
 `LCOM = |P| - |Q|`  
@@ -12,28 +16,30 @@
 `Q: número de pares de métodos que sí comparten atributos de instancia`  
 
 Funciones funcionalmente relacionadas :
--   `addEmployee` y `removeEmployee`
--   `addEmployee` y `printDepartmentReport`
+- La clase no tiene atributos de instancia
 
-
-Funciones funcionalmente no relacionadas:
-- `addEmployee` y `changeDepartment`
-- `addEmployee` y `printAllDepartments`
-- `removeEmployee` y `changeDepartment`
-- `removeEmployee` y `printDepartmentReport`
-- `removeEmployee` y `printAllDepartments`
-- `changeDepartment` y `printDepartmentReport`
-- `changeDepartment` y `printAllDepartments`
-- `printDepartmentReport` y `printAllDepartments`
+Funciones de la clase:
+- `addEmployee`
+- `removeEmployee`  
+- `changeDepartment`
+- `printDepartmentReport`
+- `printAllDepartments`
+Que forman 10 pares dos a dos.
 
 >Aplicando un `count()` mental tenemos:  
-P = 8  
-Q = 2  
-LCOM = 8-2 = 6
+P = 10  
+Q = 0  
+LCOM = 10-0 = 10, este valor de LCOM denota baja cohesión y se explica porque todos los métodos son independientes entre síy no comparten atributo de instancia.
 
-**Métricas de Acoplamiento**  
-**Instability**
+**Métricas de Acoplamiento**   
+El [acoplamiento](https://univirtual.uni.pe/pluginfile.php/628866/mod_resource/content/3/Metricas-calidad.pdf) se refiere a la medida en que los módulos de un sistema de software dependen unos de otros. Un bajo acoplamiento es deseable porque reduce la complejidad y mejora la mantenibilidad del
+sistema. Las métricas de acoplamiento ayudan a identificar las dependencias entre los componentes del
+software y evalúan la calidad del diseño.
 
+**Instability**  
+La inestabilidad de un módulo se mide para entender la volatilidad potencial de un módulo dentro de la
+arquitectura del software. Indica la probabilidad de que un módulo se vea afectado por cambios en
+otros módulos.
 >[!NOTE]
 `I = Ce / (Ca + Ce)`  
 `Ce: Número de clases externas utilizadas por la clase actual`  
@@ -50,6 +56,8 @@ Ce = 2
 Ca = 0  
 I = 2/(2+0) = 1  
 Concluimos que nuestra clase es completamente inestable y se recomienda dividirla en más clases.
+
+### Análisis luego de refactorizar la clase
 
 ## Ejercicio 2
 
